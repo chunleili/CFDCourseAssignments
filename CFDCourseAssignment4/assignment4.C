6d3ca1d2fa3aac91938d303e1a454e711165bf95
chunleili <lichunlei1996@126.com>
2019-07-10 21:54:15 +0800

    7/10 晚上

--- CFDCourseAssignment4/assignment4.C
+++ CFDCourseAssignment4/assignment4.C
@@ -7,33 +7,35 @@
 #include<cstdlib>
 using namespace std;
 
-pair<int,int> genMesh();
+void genMesh();
 void init1();
 void init2();
 double localTimeStepping();
 void solve();
-void print();
+void print(field aField, string filename);
 
 /***************************define the consts ********************************/
 
 const double stop=1.0;
 const int stopStep=1000;
+const int maxI=400, maxJ=100;
 
 /***************************define the type ********************************/
 typedef struct xy
 {
     public:
-    double x;
-    double y;
+    double x=0;
+    double y=0;
 }xy;
 
+typedef vector<vector<xy> > meshPoint;
+typedef vector<vector<double> > field;
 /********************************main()**************************************/
 int main()
 {  
-    pair<int,int> maxIJ=genMesh();              //生成网格
-    double maxI=maxIJ.first, maxJ=maxIJ.second;
+    genMesh();              //生成网格
     
-    vector<vector<double> > Q, F;
+    field Q, F;
     
     cout<<"please input case Number: 1 for inlet 1.5Ma; 2 for inlet 1.8Ma"<<endl;
     int caseNo;
@@ -52,23 +54,58 @@ int main()
         solve();                  //求解
     }
 
-    print();                //打印 
+    print(Q,"Q.txt");                //打印 
     return 0;
 }
 
 /********************************funcs define**************************************/
-pair<int,int> genMesh()
+void genMesh()
 {
-
-    const int maxI=400, maxJ=100;
     const int block1=100, block2=100, block3=maxI-block1-block2;
+    const double dx=1/400;
 
-    double mesh[maxI][maxJ];
+    meshPoint mesh(maxI+1, vector<xy>(maxJ+1));
+
+    double dy=0.3/maxJ;
+    for(int i=0; i<=block1; i++)
+    {
+        for(int j=0;j<=maxJ; j++)
+        {
+            mesh[i][j].x=i*dx;
+            mesh[i][j].y=j*dy;
+        }
+    }
+    
+    for(int i=block1+1; i<=block1+block2; i++)
+    {
+        double h=0.25*i*dx-0.05;
+        dy=(0.3-h)/maxJ;
+        for(int j=0;j<=maxJ; j++)
+        {
+            mesh[i][j].x=i*dx;
+            mesh[i][j].y=h+j*dy;
+        }
+    }
+
+    dy=(0.3-0.05)/maxJ;
+    for(int i=block1+block2+1; i<=maxI; i++)
+    {
+        for(int j=0;j<=maxJ; j++)
+        {
+            mesh[i][j].x=0.05+i*dx;
+            mesh[i][j].y=j*dy;
+        }
+    }
+
     fstream fout("mesh.txt");
-    //for(auto &i: mesh)
-        
+    for(int j=0; j<=maxJ; j++)
+    {
+        for(int i=0; i<=maxI; i++)
+        {
+            fout<< mesh[i][j].x<<" "<<mesh[i][j].y<<endl;
+        }
+    }
 
-    return make_pair(maxI,maxJ);
 }
 
 void init1()
@@ -92,7 +129,14 @@ void solve()
 
 }
 
-void print()
+void print(field aField, string filename)
 {
-
+    fstream fout(filename);
+    for(int j=0; j<=maxJ; j++)
+    {
+        for(int i=0; i<=maxI; i++)
+        {
+            //fout<< aField[i][j].x<<" "<<aField[i][j].y<<endl;
+        }
+    }
 }
